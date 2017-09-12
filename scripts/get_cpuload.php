fc080cbf14180e891dc2cde540cdce6394ad77c5
Rocco Pierpaolo Sicilia (aka: BrC) <rocco.sicilia@gmail.com>
2017-09-12 23:45:21 +0200

    fix

--- scripts/get_cpuload.php
+++ scripts/get_cpuload.php
@@ -3,12 +3,13 @@
 <?php
 
 $output_uptime = exec("uptime"); // get cpuload avarage 1-5-15
-$output_uptime = explode(",", $output_uptime);
+$output_uptime = str_replace(',', '', $output_uptime);
+$output_uptime = explode(' ', $output_uptime);
 
 $cpu_15m = array_pop($output_uptime);
 $cpu_05m = array_pop($output_uptime);
 $cpu_01m = array_pop($output_uptime);
 
-echo $cpu_01m.", ".$cpu_05m.", ".$cpu_01m."\n";
+echo $cpu_01m.", ".$cpu_05m.", ".$cpu_15m."\n";
 
 ?>
